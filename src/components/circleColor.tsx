0110e93f0b910c9c92273f491a3de2874e991ae8
Fadi-Maher <cleverfady@gmail.com>
2024-11-26 21:10:53 +0200

    handle build

--- src/components/circleColor.tsx
+++ src/components/circleColor.tsx
@@ -8,6 +8,8 @@ interface Iprops extends HTMLAttributes<HTMLSpanElement> {
 
 
 
+
+
 const CircleColor = ({color ,  ...rest }: Iprops) => {
     return (
         <span className="block w-5 h-5 rounded-full cursor-pointer" style={{backgroundColor:color}}   {...rest} />
